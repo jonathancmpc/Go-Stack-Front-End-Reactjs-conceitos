388d5a4fd9195ea543de6127b2b7209dfdae6646
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-06-18 17:27:33 -0300

    Babel,Webpack,Componentes,Propriedades,Estado e Imutabilidade

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+
+function App(){
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject(){
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  }
+
+  return (
+    <>
+      <Header title="Homepage" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path'); /* Importando módulo do Node para utilizar os path's, ele serve para não dar problema nas barras e contra-barras dos sistemas operacionais, como windows por exemplo */
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'), /* Arquivo de entrada da nossa aplicação, primeiro arquivo carregado */
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  }, /* Qual arquivo será gerado depois de convertido(bundle) */
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'), /* Servidor do webpack para ficar escutando qualquer alteração dos nossos arquivos que estão na pasta pública */
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:{
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+};
